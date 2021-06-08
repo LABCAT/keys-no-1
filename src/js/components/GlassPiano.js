79714926887578913f90f8de3b8f027d73a369be
LABCAT <shane.watters.nz@gmail.com>
2021-06-09 09:29:33 +1200

    Improving loading experience.

--- src/js/components/GlassPiano.js
+++ src/js/components/GlassPiano.js
@@ -1,13 +1,18 @@
-import React  from "react";
-import { useGLTF } from "@react-three/drei";
-
-export default function GlassPiano(props) {
-  const { nodes } = useGLTF("/GlassPiano.gltf");
-
-  return (
-     <primitive object={nodes['OSG_Scene']} position={[-1.5,0,0]} />
-  );
-}
+import React, { useEffect, useState } from 'react'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 //https://sketchfab.com/3d-models/glass-piano-8e333a3b1220494a8db27f68509715f2
-useGLTF.preload("/GlassPiano.gltf");
+export default function GlassPiano(props) {
+  const url = '/GlassPiano.gltf';
+  const [model, setModel] = useState();
+  const { nodes } = useLoader(GLTFLoader, url, (loader) => {})
+
+  useEffect(() => {
+    if (!model) {
+      setModel(nodes);
+      document.getElementById("play-icon").classList.add("show");
+    }
+  }, []);
+  return <primitive object={nodes['OSG_Scene']} position={[-1.5,0,0]} />
+}
