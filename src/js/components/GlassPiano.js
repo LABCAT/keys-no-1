9be7fd659071580ee529daefb4eb125f3a4f4184
LABCAT <shane.watters.nz@gmail.com>
2021-06-10 08:33:57 +1200

    Fixing useEffect errors.

--- src/js/components/GlassPiano.js
+++ src/js/components/GlassPiano.js
@@ -5,14 +5,14 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 //https://sketchfab.com/3d-models/glass-piano-8e333a3b1220494a8db27f68509715f2
 export default function GlassPiano(props) {
   const url = '/GlassPiano.gltf';
-  const [model, setModel] = useState();
+  const [loaded, setLoaded] = useState();
   const { nodes } = useLoader(GLTFLoader, url, (loader) => {})
 
   useEffect(() => {
-    if (!model) {
-      setModel(nodes);
+    if (!loaded) {
+      setLoaded(true);
       document.getElementById("play-icon").classList.add("show");
     }
-  }, []);
+  }, [loaded]);
   return <primitive object={nodes['OSG_Scene']} position={[-1.5,0,0]} />
 }
